565df5ef9eaf9c6b4a7ee6ea5adde0f0f9f85d80
not ervin <notminin@pm.me>
2019-07-06 19:57:09 +0200

    it works:tm:

--- index.js
+++ index.js
@@ -5,18 +5,45 @@ if (!process.stdin.isTTY) {
 process.stdin.setRawMode(true);
 process.stdin.resume();
 
+let data = "";
+let cursor = 0;
+
+render();
+
 process.stdin.on("data", (key) => {
+	for (let i = 0; i < Buffer.byteLength(key); i++) {
+		onkey(key.slice(i, i + 1));
+	}
+});
+
+function onkey(key) {
 	let hex = parseInt(key.toString("hex"), 16);
+
 	switch (hex) {
 		case 0x3: { // EOL
 			console.log("bye :c");
 			process.exit();
 		} break;
 
+		case 0x7F: {
+			data = data.substring(0, data.length - 1);
+		} break;
+
 		default: {
-			if (hex >= 0x20 && hex <= 0x7e) {
-				process.stdout.write(key.toString());
+			if ((hex >= 0x20 && hex <= 0x7e) || hex == 0x9) {
+				data += key.toString();
+			} else if (hex == 0xD) {
+				data += "\n";
 			}
 		} break;
 	}
-});
+
+	render();
+}
+
+function render() {
+	process.stdout.write('\x1b[2J');
+	process.stdout.write('\x1b[0f');
+
+	process.stdout.write(data);
+}
