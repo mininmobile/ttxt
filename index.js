e2ac0bfa42dab3a278898601503382da52a683ab
not ervin <notminin@pm.me>
2019-07-06 18:55:22 +0200

    write most characters

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ process.stdin.on("data", (key) => {
 			console.log("bye :c");
 			process.exit();
 		} break;
+
+		default: {
+			if (hex >= 0x20 && hex <= 0x7e) {
+				process.stdout.write(key.toString());
+			}
+		} break;
 	}
 });
