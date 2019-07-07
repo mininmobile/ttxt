21bbab24617d79e6624a5a673b2c505401c3de65
not ervin <notminin@pm.me>
2019-07-07 10:27:58 +0200

    add resize

--- index.js
+++ index.js
@@ -19,11 +19,17 @@ let data = "";
 
 render(true);
 
-process.stdin.on("data", (key) => {
-	for (let i = 0; i < Buffer.byteLength(key); i++) {
-		onkey(key.slice(i, i + 1));
-	}
-});
+{ // event listeners
+	process.stdin.on("data", (key) => {
+		for (let i = 0; i < Buffer.byteLength(key); i++) {
+			onkey(key.slice(i, i + 1));
+		}
+	});
+
+	process.stdout.on("resize", () => {
+		render(true);
+	});
+}
 
 function onkey(key) {
 	let recache = false;
@@ -37,6 +43,9 @@ function onkey(key) {
 		} break;
 
 		case 0x7F: { // BS
+			if (data.substring(data.length - 1) == "\n")
+				recache = true;
+
 			data = data.substring(0, data.length - 1);
 		} break;
 
@@ -54,39 +63,42 @@ function onkey(key) {
 }
 
 function render(recache = false) {
+	let lines = data.split("\n");
+	let margin = 4 + lines.length.toString().length;
+
 	process.stdout.write("\x1b[2J");
 	process.stdout.write("\x1b[0f");
 
 	if (recache) {
 		// first ui line
-		let _line0 = `─────┬${"─".repeat(process.stdout.columns - 6)}`;
+		let _line0 = `${"─".repeat(margin)}┬${"─".repeat(process.stdout.columns - margin - 1)}`;
 		cache.line0 = colorize.black(_line0);
 
 		// second ui line
-		let _line1 = colorize.black(`     │`);
+		let _line1 = colorize.black(`${" ".repeat(margin)}│`);
 		cache.line1 = `${_line1} editing: ${"untitled"}\n`;
 
 		// third ui line
-		let _line2 = `─────┼${"─".repeat(process.stdout.columns - 6)}`;
+		let _line2 = `${"─".repeat(margin)}┼${"─".repeat(process.stdout.columns - margin - 1)}`;
 		cache.line2 = colorize.black(_line2);
 
 		// fourth ui line
-		let _line3 = `─────┴${"─".repeat(process.stdout.columns - 6)}`;
+		let _line3 = `${"─".repeat(margin)}┴${"─".repeat(process.stdout.columns - margin - 1)}`;
 		cache.line3 = colorize.black(_line3);
 	}
 
 	process.stdout.write(cache.line0 + cache.line1 + cache.line2);
 
-	// code
-	let lines = data.split("\n");
-
 	for (let i = 0; i < process.stdout.rows - 4; i++) {
 		if (lines[i] != undefined) {
-			process.stdout.write(`${colorize.black(`  ${i + 1}  │`)} ${lines[i]}\n`);
+			let left = " ".repeat((lines.length.toString().length - (i + 1).toString().length) + 2);
+			process.stdout.write(`${colorize.black(`${left}${i + 1}  │`)} ${lines[i]}\n`);
 		} else {
-			process.stdout.write(colorize.black(`     │\n`));
+			process.stdout.write(colorize.black(`${" ".repeat(margin)}│\n`));
 		}
 	}
 
 	process.stdout.write(cache.line3);
+
+	process.stdout.cursorTo(0, 0);
 }
